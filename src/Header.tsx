25894338d75c585223776df3b925d27018b09aba
Dave <dave.lee@stagefive.com>
2023-03-30 12:28:07 +0900

    - feat: 프로젝트 환경설정

---
+++ src/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => (
+  <div className="flex flex-1 py-4">
+    <p>{`Nayoung Han\nData Scientist`}</p>
+    <div className="ml-4 flex flex-1 justify-between px-24">
+      <span>Home</span>
+      <span>About</span>
+      <span>Projects</span>
+      <span>Recent Post</span>
+    </div>
+  </div>
+);
+
+export default Header;
