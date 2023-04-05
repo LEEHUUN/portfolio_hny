dfcc7d563e06efc187c8916403d6c35aec3b2e3c
Dave <dave.lee@stagefive.com>
2023-04-05 15:12:09 +0900

    - feat: emotion setting

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,8 +1,14 @@
+import { css } from "@emotion/react";
+
+const cssTest = () => css`
+  font-size: 34px;
+`;
+
 const Header = () => (
   <div className="flex flex-1 py-4">
     <p>{`Nayoung Han\nData Scientist`}</p>
     <div className="ml-4 flex flex-1 justify-between px-24">
-      <span>Home</span>
+      <span css={cssTest}>Home</span>
       <span>About</span>
       <span>Projects</span>
       <span>Recent Post</span>
