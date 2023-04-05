64d3c4603415a6ef89d03e75adb9f5235c193c77
Dave <dave.lee@stagefive.com>
2023-04-05 16:11:07 +0900

    - feat: font poppins 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link href="https://fonts.cdnfonts.com/css/poppins" rel="stylesheet" />
+      </Head>
       <body className="bg-white	">
         <Main />
         <NextScript />
