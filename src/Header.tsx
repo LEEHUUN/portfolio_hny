d67f30f0a46e8f323c2dfaf7d4170b91beac5141
Dave <dave.lee@stagefive.com>
2023-04-05 16:11:43 +0900

    - feat: header css 적용

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,17 +1,42 @@
 import { css } from "@emotion/react";
+import Link from "next/link";
 
-const cssTest = () => css`
-  font-size: 34px;
+const containerStyle = () => css`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 80px;
+`;
+
+const logoStyle = () => css`
+  font-size: 20px;
+  font-family: Poppins;
+  white-space: pre-line;
+  font-weight: bold;
+  line-height: 24px;
+  color: #42446e;
+`;
+
+const linkerWrapperStyle = () => css`
+  flex: 1;
+  margin: 28px 0 0 240px;
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+  font-size: 16px;
+  font-family: Poppins;
+  font-weight: medium;
+  cursor: pointer;
 `;
 
 const Header = () => (
-  <div className="flex flex-1 py-4">
-    <p>{`Nayoung Han\nData Scientist`}</p>
-    <div className="ml-4 flex flex-1 justify-between px-24">
-      <span css={cssTest}>Home</span>
-      <span>About</span>
-      <span>Projects</span>
-      <span>Recent Post</span>
+  <div css={containerStyle}>
+    <Link href={"#"} css={logoStyle}>{`Nayoung Han\nAI Researcher`}</Link>
+    <div css={linkerWrapperStyle}>
+      <Link href={"#"}>Home</Link>
+      <Link href={"#"}>About</Link>
+      <Link href={"#"}>Projects</Link>
+      <Link href={"#"}>Recent Post</Link>
     </div>
   </div>
 );
